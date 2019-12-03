42721b061e5b2596a845a3ea295a9cb385fa1359
Waseem <idelbiw@gmail.com>
2019-12-02 22:44:27 -0500

    let's test it out!

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -2,4 +2,21 @@ import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
     
+    var x: UInt
+    var y: UInt
+    
+    for number: UInt in 1...999999999999999 {
+        for secondNumber: UInt in 1...999999999999999 {
+            
+            if number + secondNumber == sum && number * secondNumber == product {
+                x = number
+                y = secondNumber
+                return [x, y]
+            }
+        }
+    }
+    return []
 }
+    
+
+sumAndProduct(6, 9)
